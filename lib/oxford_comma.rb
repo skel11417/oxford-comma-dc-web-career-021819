6afdca9517cc4594b80a1fc7a315a9676321d8e9
Sean Kelly <shonkelli@gmail.com>
2019-02-04 23:18:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
     array.join(" and ")
   elsif num > 2
     last = array.pop
-    array.join(", ")
-    array << ", and #{last}"
+    output = array.join(", ")
+    output << ", and #{last}"
   end
 end
